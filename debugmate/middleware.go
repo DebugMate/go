cdf76b62e0775d5a144a6e9d6522aea05ae6d522
Luis Felipe <luis.santos@devsquad.com>
2024-09-13 10:41:06 -0300

    change middleware to catch http errors

--- debugmate/middleware.go
+++ debugmate/middleware.go
@@ -5,26 +5,51 @@ import (
 	"net/http"
 )
 
-func Recoverer(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if rvr := recover(); rvr != nil {
-				if rvr == http.ErrAbortHandler {
-					panic(rvr)
-				}
+type responseWriter struct {
+	http.ResponseWriter
+	status  int
+	written bool
+}
 
-				err := fmt.Errorf("%v", rvr)
-
-				Catch(err)
-
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte("Internal Server Error"))
-
-			}
-		}()
-
-		next.ServeHTTP(w, r)
+func (rw *responseWriter) WriteHeader(code int) {
+	if !rw.written {
+		rw.status = code
+		rw.ResponseWriter.WriteHeader(code)
+		rw.written = true
 	}
+}
 
-	return http.HandlerFunc(fn)
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	if !rw.written {
+		rw.WriteHeader(http.StatusOK)
+	}
+	return rw.ResponseWriter.Write(b)
+}
+
+func Recoverer(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        rw := &responseWriter{ResponseWriter: w, status: http.StatusOK}
+        
+        defer func() {
+            if rvr := recover(); rvr != nil {
+                if rvr == http.ErrAbortHandler {
+                    panic(rvr)
+                }
+
+                err := fmt.Errorf("Panic recovered: %v", rvr)
+                Catch(err)
+
+                rw.WriteHeader(http.StatusInternalServerError)
+                rw.Write([]byte("Internal Server Error"))
+            }
+
+            // Capturar todos os erros HTTP (4xx e 5xx)
+            if rw.status >= 400 {
+                err := fmt.Errorf("HTTP error: %d %s", rw.status, http.StatusText(rw.status))
+                Catch(err)
+            }
+        }()
+
+        next.ServeHTTP(rw, r)
+    })
 }
